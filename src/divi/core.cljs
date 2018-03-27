d71d2f505372161a7047700296cbc66a29c26215
Ville Svärd <ville.svard@agical.se>
2018-03-27 08:05:38 +0200

    style -> css

--- src/divi/core.cljs
+++ src/divi/core.cljs
@@ -13,71 +13,56 @@
 
 (defn mata-in [[c r]]
   (fn [e]
-    (.log js/console e)
-    (.preventDefault e) 
-    (let [nytt-värde (-> e
-                         .-target
-                         .-value)]
-      (swap! app-state 
-             assoc-in
-             [:data c r]
-             nytt-värde))
+    (when-not (:visa-svar @app-state)
+      (let [nytt-värde (-> e
+                           .-target
+                           .-value)]
+        (swap! app-state 
+               assoc-in
+               [:data c r]
+               nytt-värde)))
     nil))
 
-(defn multiplikationstabell []
-  [:table {:style {:font-family "monospace"}}
+(defn cell-value-for [state [c r]]
+  (if (:visa-svar state)
+    (* c r)
+    (get-in state
+            [:data c r] "")))
+
+(defn multiplikationstabell [state]
+  [:table
    [:tbody
-    (concat (doall (map (fn [r]
-                          [:tr {:key (str "*" r)}
-                           (cons
-                            [:td {:style {:color :red
-                                          :text-align "center"
-                                          :font-weight :bold}
-                                  :key (str r)}
-                             r]
-                            (doall (map (fn [c]
-                                          [:td {:key (str c "*" r)
-                                                :style {:width "2rem"
-                                                        :text-align "right"
-                                                        :border "solid"
-                                                        :border-width "1px"}}
-                                           (if (:visa-svar @app-state)
-                                             [:div {:style {:height "18.25px"
-                                                            :width "100%"}}
-                                              (* c r)]
-                                             [:input {:type :text
-                                                      :value (get-in @app-state
-                                                                     [:data c r] "")
-                                                      :on-input (mata-in [c r])
-                                                      :style {:padding "0px"
-                                                              :height "17px"
-                                                              :width "100%"}}])])
-                                        (range 1 11))))])
-                        (range 10 0 -1)))
+    (concat (map (fn [r]
+                   [:tr {:key (str "*" r)}
+                    (cons
+                     [:td.column {:key (str r)}
+                      r]
+                     (map (fn [c]
+                            [:td.cell {:key (str c "*" r)}
+                             [:input {:type :text
+                                      :size "2"
+                                      :on-input (mata-in [c r])
+                                      :value (cell-value-for state [c r])}]])
+                          (range 1 11)))])
+                 (range 10 0 -1))
             [[:tr {:key "*0"}
               (cons
-               [:td {:style {:color :red
-                             :font-weight :bold}
-                     :key "origo"}
-                0]
+               [:td.origo {:key "origo"} 0]
                (map (fn [c]
-                      [:td {:style {:color :red
-                                    :text-align "center"
-                                    :font-weight :bold}
-                            :key (str c "*")}
-                       c])
+                      [:td.bottom-row {:key (str c "*")} c])
                     (range 1 11)))] ])]])
 
 (defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Detta är multiplikationstabellen"]
-   [:div
-    [:input {:type "checkbox" :id "visa"
-             :checked (:visa-svar @app-state)
-             :on-change #(swap! app-state update :visa-svar not)}]
-    [:label {:for "visa"}] "Visa svaren"]
-   [multiplikationstabell]])
+  (let [state @app-state]
+    [:div
+     [:h1 (:text state)]
+     [:h3 "Detta är multiplikationstabellen"]
+     [:div
+      [:input {:type "checkbox" :id "visa"
+               :checked (:visa-svar state)
+               :on-change #(swap! app-state update :visa-svar not)}]
+      [:label {:for "visa"}] "Visa svaren"]
+     [multiplikationstabell state]]))
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
